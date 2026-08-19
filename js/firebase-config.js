4e2f347dc7859ff2d0a10b93ea6d0afa82a43037
luzEnails15-create <luz.nails15@gmail.com>
2026-08-18 22:41:02 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBAF5D32FN40O4w8IyNMSkBBVUZP90uDCc",
+export const firebaseConfig = {
+  apiKey: "AIzaSyBAF5D32FN4BO4w8IyNMSkBBVUZP90uDCc",
   authDomain: "unas-luze.firebaseapp.com",
   projectId: "unas-luze",
   storageBucket: "unas-luze.firebasestorage.app",
   messagingSenderId: "376833888552",
   appId: "1:376833888552:web:ba48f20a594e8d67ce801d"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
