a65e620ccec1b0640538103e3b3a9bd9934d3567
luzEnails15-create <luz.nails15@gmail.com>
2026-08-18 23:09:42 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBAF5D32FN40O4w8IyNMSkBBVUZP90uDCc",
   authDomain: "unas-luze.firebaseapp.com",
   projectId: "unas-luze",
