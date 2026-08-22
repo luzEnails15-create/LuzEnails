16a56d05c8c300232b2d2dda7c547a6841c19673
luzEnails15-create <luz.nails15@gmail.com>
2026-08-21 20:50:49 -0500

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBAF5D32FN40O4w8IyNMSkBBVUZP90uDCc",
-  authDomain: "unas-luze.firebaseapp.com",
-  projectId: "unas-luze",
-  storageBucket: "unas-luze.firebasestorage.app",
-  messagingSenderId: "376833888552",
-  appId: "1:376833888552:web:ba48f20a594e8d67ce801d"
-};
